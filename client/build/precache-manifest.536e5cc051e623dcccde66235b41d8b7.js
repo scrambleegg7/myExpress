65400f556bb6f7c81c0cf026a6d4d7112d6fd940
scrambleegg7 <scrambleegg7@gmail.com>
2020-05-14 11:57:42 +0900

    initial version

---
+++ client/build/precache-manifest.536e5cc051e623dcccde66235b41d8b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d4c73609a64c24a1fc803f83952fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e53c0c9c841a3ad75b9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cedfd62351dfd0248527",
+    "url": "/static/js/2.0830fd72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0830fd72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e53c0c9c841a3ad75b9",
+    "url": "/static/js/main.3eeb95ba.chunk.js"
+  },
+  {
+    "revision": "cb8f9da77ed548344868",
+    "url": "/static/js/runtime-main.c54ad692.js"
+  }
+]);
