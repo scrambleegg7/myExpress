55b408362608f619bd934c0264d85942e559bcf8
donchan <scrambleegg7@gmail.com>
2020-05-20 23:51:17 +0900

    double http and https server running, also necessary to generate self certificated.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const generatePassword = require('password-generator');
 
 const app = express();
 
+const http = require( 'http' );
+const https = require( 'https' );
+const fs = require( 'fs' );
+
 const morgan = require('morgan');
 
 const mongoose = require('mongoose');
@@ -13,7 +17,7 @@ dotenv.config();
 var Course = require('./models/course');
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 // mongo DB
 mongoose.connect(
@@ -29,6 +33,13 @@ mongoose.connect(
 mongoose.connection.on("error", err => {
 console.log(`mongoDB connection error: ${err.message}`)
 });
+
+//app.use( (req, res, next) => {
+//  res.header("Access-Control-Allow-Origin", "https://localhost"); // update to match the domain you will make the request from
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//  next();
+//});
+
 
 
 app.get('/course', (req, res) => {
@@ -64,11 +75,31 @@ app.get('/api/passwords', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//});
+
+
+var options = {
+  key: fs.readFileSync( '../../keys/server_key.pem' ),
+  cert: fs.readFileSync( '../../keys/server_crt.pem' )
+};
+const port = process.env.PORT || 5000;
+
+
+var serverhttps = https.createServer( options, app )
+.listen( port, () => {
+    console.log( "https server stating on " + port + " ..." );
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+var serverhttp = http.createServer(  app )
+.listen( 5001, () => {
+    console.log( "http server stating on " + 5001 + " ..." );
+});
+
+
+
+
+//app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
